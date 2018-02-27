010c184146f2445b71288cf1e3c991f3061b2e97
DmitryToropov <toropov85@mail.ru>
2018-02-27 15:57:26 +0300

    demo

--- index.php
+++ index.php
@@ -9,6 +9,11 @@
     <?php require_once "blocks/header.php" ?>
     <?php require_once "blocks/head.php" ?>
     <?php require_once "blocks/footer.php" ?>
+    <div class="copyright">
+        <div class="wrapper">
+            &copy; 2018 Дмитрий Лаврик - персональный сайт
+        </div>
+    </div>
     <script></script>
 </body>
 </html>
