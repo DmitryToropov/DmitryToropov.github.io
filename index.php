9c1d144b40ca2f861ca1548e0131c477128bb98e
DmitryToropov <toropov85@mail.ru>
2018-02-27 13:17:03 +0300

    demo

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html leng="en">
+<head>
+	<meta charset="UTF-8">
+    <title>My first page</title>
+    <link href="style/style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+    <?php require_once 'blocks/header.php' ?>
+    <?php require_once 'blocks/head.php' ?>
+    <?php require_once 'blocks/footer.php' ?>
+    <script></script>
+</body>
+</html>
